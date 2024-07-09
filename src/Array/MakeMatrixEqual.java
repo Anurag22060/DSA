3542671aa8deba1f206ad43799b712e1dd09395a
Anurag22060 <anuragsawal03@gmail.com>
2024-07-09 10:12:49 +0530

    Codes added

---
+++ src/Array/MakeMatrixEqual.java
@@ -0,0 +1,47 @@
+package Array;
+
+import java.util.Arrays;
+
+public class MakeMatrixEqual {
+    public static void main(String[] args) {
+        int a[][]={{3,63,42},
+                   {18,12,12},
+                   {15,21,18},
+                   {33,84,24}};
+        int k = 3;
+        System.out.println(makeMatrixEqual(a,k));
+    }
+
+    static int makeMatrixEqual(int a[][],int k){
+        int n = a.length;
+        int m = a[0].length;
+        if(n ==0) return  0;
+        int total = n * m;
+        int b[] = new int[total];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                b[i * m + j] = a[i][j];
+
+            }
+        }
+        Arrays.sort(b);
+        int median = b[total / 2];
+
+//        For analysing values below loop can be used.
+
+//        for (int i = 0; i < total; i++) {
+//            System.out.println(b[i] + " " + makeMatixHelper(b,b[i],k));
+//        }
+
+        return makeMatixHelper(b,median,k);
+    }
+
+    static int makeMatixHelper(int b[],int m,int k){
+        int count = 0;
+        for (int i = 0; i < b.length; i++) {
+            count += Math.abs(b[i] - m)/ k;
+        }
+        return count;
+    }
+}
