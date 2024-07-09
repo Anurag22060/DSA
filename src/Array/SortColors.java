3a979b3bc82d89fa1a4846908c72626fd86c9cff
Anurag22060 <anuragsawal03@gmail.com>
2024-07-09 21:15:15 +0530

    Codes added

---
+++ src/Array/SortColors.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class SortColors {
+    public static void main(String[] args) {
+        int a[] = {2,1,2,1,0,0,2,1,0};
+        sortArray(a);
+        for(int e :a){
+            System.out.print(e+" ");
+        }
+    }
+
+    static void sortArray(int a[]){
+        int low = 0;
+        int mid = 0;
+        int high =a.length-1;
+
+        while(mid<= high){
+            if(a[mid] == 2){
+                a[mid] = a[high];
+                a[high] = 2;
+                high --;
+            }
+            else if (a[mid] == 1) {
+                mid++;
+            } else if (a[mid]==0) {
+                a[mid] = a[low];
+                a[low] = 0;
+                low++;
+                mid++;
+            }
+        }
+    }
+
+}
